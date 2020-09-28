84dabcc3c94450addc8d51ba1c312bc350d8a9af
Andi Cirjaliu <Andi.Cirjaliu@ro.ibm.com>
2020-09-29 02:01:44 +0300

    Build app

---
+++ build/precache-manifest.a86e084153ca0a75929c47a936648c2f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e9733a459a841317f000d8b2fcc38b5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f3bc29e8a5fd7b6cdeab",
+    "url": "/static/css/main.620fe750.chunk.css"
+  },
+  {
+    "revision": "d913d4360556edbe93e0",
+    "url": "/static/js/2.b6056a3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b6056a3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f3bc29e8a5fd7b6cdeab",
+    "url": "/static/js/main.53de8877.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
